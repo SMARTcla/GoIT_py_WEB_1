a0ed3fcc91fb057345d202b78b3407a34f92f0a3
Mikhailo Kononenko <93360760+SMARTcla@users.noreply.github.com>
2022-07-06 18:42:25 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,30 @@
+class Meta(type):
+    
+    class_num = 0
+    
+    def __new__(*args):
+        instance = type.__new__(*args)
+        instance.class_number = Meta.class_num
+        Meta.class_num += 1
+        return instance
+
+
+#Meta.children_number = 0
+
+
+class Cls1(metaclass=Meta):
+    
+    def __init__(self, data):
+        self.data = data
+
+
+class Cls2(metaclass=Meta):
+    
+    def __init__(self, data):
+        self.data = data
+
+assert (Cls1.class_number, Cls2.class_number) == (0, 1)
+
+a, b = Cls1(''), Cls2('')
+
+assert (a.class_number, b.class_number) == (0, 1)
